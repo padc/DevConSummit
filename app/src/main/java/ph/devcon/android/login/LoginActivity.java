c8335cb43fe7aa9074e77e7e719dbe9423d08485
Lope Emano <lopegwapo@gmail.com>
2014-10-19 19:55:18 +0800

    improved login layout

--- app/src/main/java/ph/devcon/android/login/LoginActivity.java
+++ app/src/main/java/ph/devcon/android/login/LoginActivity.java
@@ -2,9 +2,14 @@ package ph.devcon.android.login;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
 import ph.devcon.android.R;
 import ph.devcon.android.navigation.MainActivity;
@@ -13,6 +18,15 @@ import ph.devcon.android.navigation.MainActivity;
  * Created by lope on 9/16/14.
  */
 public class LoginActivity extends Activity {
+
+    @InjectView(R.id.edt_email_address)
+    EditText edtEmailAddress;
+
+    @InjectView(R.id.edt_password)
+    EditText edtPassword;
+
+    @InjectView(R.id.btn_login)
+    Button btnLogin;
 
     @OnClick(R.id.btn_login)
     public void onClickLogin(View view) {
@@ -24,6 +38,16 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.inject(this);
+        init();
+    }
+
+    protected void init() {
+        Typeface credentialsFont = Typeface.createFromAsset(getAssets(), "fonts/SourceSansPro-Light.otf");
+        edtEmailAddress.setTypeface(credentialsFont);
+        edtPassword.setTypeface(credentialsFont);
+        Typeface buttonFont = Typeface.createFromAsset(getAssets(), "fonts/SourceSansPro-Bold.otf");
+        btnLogin.setTypeface(buttonFont);
     }
 
 }
