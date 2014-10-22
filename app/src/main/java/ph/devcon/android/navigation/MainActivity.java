4234734327bd6edc0c208dac11f114443bde4802
Lope Emano <lopegwapo@gmail.com>
2014-10-22 18:26:44 +0800

    navigation drawer opens automatically, ic_launcher asset change

--- app/src/main/java/ph/devcon/android/navigation/MainActivity.java
+++ app/src/main/java/ph/devcon/android/navigation/MainActivity.java
@@ -1,8 +1,17 @@
 package ph.devcon.android.navigation;
 
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+
 import ph.devcon.android.R;
 
 public class MainActivity extends BaseDevConActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDrawerLayout.openDrawer(GravityCompat.START);
+    }
 
     @Override
     protected int getLayout() {
