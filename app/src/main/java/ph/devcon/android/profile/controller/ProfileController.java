56e5bb04b3eb482043d4f3da2d51aac1b18a881c
Lope Emano <lopegwapo@gmail.com>
2014-11-04 23:35:31 +0800

    prepared edit profile controller via retrofit

--- app/src/main/java/ph/devcon/android/profile/controller/ProfileController.java
+++ app/src/main/java/ph/devcon/android/profile/controller/ProfileController.java
@@ -4,7 +4,12 @@ import ph.devcon.android.profile.api.EditProfileBaseResponse;
 import retrofit.Callback;
 import retrofit.http.Field;
 import retrofit.http.FormUrlEncoded;
+import retrofit.http.Multipart;
 import retrofit.http.POST;
+import retrofit.http.PUT;
+import retrofit.http.Part;
+import retrofit.mime.TypedFile;
+import retrofit.mime.TypedString;
 
 /**
  * Created by lope on 10/27/2014.
@@ -13,4 +18,14 @@ public interface ProfileController {
     @FormUrlEncoded
     @POST("/profile")
     void fetchProfile(@Field("authentication_token") String token, Callback<EditProfileBaseResponse> callback);
+
+    @Multipart
+    @PUT("/profile")
+    void editProfile(@Part("authentication_token") TypedString token, @Part("photo") TypedFile photo,
+                     @Part("email") TypedString email, @Part("primary_technology") TypedString primaryTechnology,
+                     @Part("first_name") TypedString firstName, @Part("last_name") TypedString lastName,
+                     @Part("position") TypedString position, @Part("company") TypedString company,
+                     @Part("location") TypedString location, @Part("description") TypedString description,
+                     @Part("website") TypedString website, @Part("facebook_url") TypedString facebookUrl,
+                     @Part("twitter_handle") TypedString twitterHandle, @Part("photo_url") TypedString photoUrl);
 }
